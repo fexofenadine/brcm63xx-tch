4fa2198f3192c745adda446baf4c62fe45202969
fexofenadine <pwandz@gmail.com>
2021-04-03 20:07:45 +1100

    identified packages w/ no control file, or underscores in PKG_NAME.  removed sync conflicts

---
+++ listunderscores.sh
@@ -0,0 +1,17 @@
+rm ../../../underscoredpackages.txt
+pushd source > /dev/null
+  for folder in */
+  do
+    pushd $folder  > /dev/null
+      for packageName in */
+      do
+        pushd $packageName > /dev/null
+          if cat control/control | grep Package | grep _; then
+            echo $folder$packageName | tee -a ../../../underscoredpackages.txt
+            cat control/control | grep Package*_* | grep _ >> ../../../underscoredpackages.txt
+          fi
+        popd > /dev/null
+      done
+    popd > /dev/null
+  done
+popd > /dev/null
